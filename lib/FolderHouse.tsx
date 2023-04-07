417ba5f86d50fd3b9a155405cbb82f5f22c08580
amje <amje@users.noreply.github.com>
2023-04-07 11:23:44 +0000

    feat: sync icons

--- lib/FolderHouse.tsx
+++ lib/FolderHouse.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {SVGProps} from 'react';
-const FolderHome = (props: SVGProps<SVGSVGElement>) => (
+const FolderHouse = (props: SVGProps<SVGSVGElement>) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width={16}
@@ -17,4 +17,4 @@ const FolderHome = (props: SVGProps<SVGSVGElement>) => (
         />
     </svg>
 );
-export default FolderHome;
+export default FolderHouse;
