70222c3b5e8f9339d2b6bf17fa033e93d33efda0
amje <amje@users.noreply.github.com>
2023-10-31 10:41:58 +0000

    feat: sync icons

--- lib/BarsAscendingAlignLeftArrowDown.tsx
+++ lib/BarsAscendingAlignLeftArrowDown.tsx
@@ -12,15 +12,8 @@ const BarsAscendingAlignLeftArrowDown = (props: SVGProps<SVGSVGElement>) => (
         <path
             fill="currentColor"
             fillRule="evenodd"
-            d="M7.75 13.5a.75.75 0 0 1 0-1.5h7.5a.75.75 0 0 1 0 1.5h-7.5ZM7 8c0 .414.336.75.75.75h5.5a.75.75 0 0 0 0-1.5h-5.5A.75.75 0 0 0 7 8Zm0-4.75c0 .414.336.75.75.75h2.5a.75.75 0 0 0 0-1.5h-2.5a.75.75 0 0 0-.75.75Z"
+            d="M2.22 13.28a.75.75 0 0 0 1.06 0l2-2a.75.75 0 1 0-1.06-1.06l-.72.72V3.25a.75.75 0 0 0-1.5 0v7.69l-.72-.72a.75.75 0 1 0-1.06 1.06l2 2ZM7.75 12a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5h-7.5Zm0-3.25a.75.75 0 0 1 0-1.5h5.5a.75.75 0 0 1 0 1.5h-5.5Zm0-4.75a.75.75 0 0 1 0-1.5h2.5a.75.75 0 0 1 0 1.5h-2.5Z"
             clipRule="evenodd"
-        />
-        <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1.5}
-            d="m2.75 12.75 2-2m-2 2-2-2m2 2v-9.5"
         />
     </svg>
 );
