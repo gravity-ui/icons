386882a2869881e906da9570ddebaace594c1b13
amje <amje@users.noreply.github.com>
2024-02-26 15:18:06 +0000

    feat: sync icons

--- lib/LayoutSplitColumns.tsx
+++ lib/LayoutSplitColumns.tsx
@@ -9,12 +9,19 @@ const LayoutSplitColumns = (props: SVGProps<SVGSVGElement>) => (
         viewBox="0 0 16 16"
         {...props}
     >
-        <path
-            fill="currentColor"
-            fillRule="evenodd"
-            d="M5 12.5H3a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1ZM3 14h2a2.5 2.5 0 0 0 2.5-2.5v-7A2.5 2.5 0 0 0 5 2H3A2.5 2.5 0 0 0 .5 4.5v7A2.5 2.5 0 0 0 3 14Zm10-1.5h-2a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1ZM11 14h2a2.5 2.5 0 0 0 2.5-2.5v-7A2.5 2.5 0 0 0 13 2h-2a2.5 2.5 0 0 0-2.5 2.5v7A2.5 2.5 0 0 0 11 14Z"
-            clipRule="evenodd"
-        />
+        <g clipPath="url(#a)">
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M5 12.5H3a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1ZM3 14h2a2.5 2.5 0 0 0 2.5-2.5v-7A2.5 2.5 0 0 0 5 2H3A2.5 2.5 0 0 0 .5 4.5v7A2.5 2.5 0 0 0 3 14Zm10-1.5h-2a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1ZM11 14h2a2.5 2.5 0 0 0 2.5-2.5v-7A2.5 2.5 0 0 0 13 2h-2a2.5 2.5 0 0 0-2.5 2.5v7A2.5 2.5 0 0 0 11 14Z"
+                clipRule="evenodd"
+            />
+        </g>
+        <defs>
+            <clipPath id="a">
+                <path fill="currentColor" d="M0 0h16v16H0z" />
+            </clipPath>
+        </defs>
     </svg>
 );
 export default LayoutSplitColumns;
