1de1620402bdb564916b29c5cbcae9e75f4aa000
amje <amje@users.noreply.github.com>
2024-05-29 09:14:05 +0000

    feat: sync icons

---
+++ lib/ArrowChevronLeft.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import type {SVGProps} from 'react';
+const ArrowChevronLeft = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={16}
+        height={16}
+        fill="none"
+        viewBox="0 0 16 16"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M14 8.75a.75.75 0 0 0 0-1.5H8.31l2.72-2.72a.75.75 0 1 0-1.06-1.06l-4 4a.75.75 0 0 0 0 1.06l4 4a.75.75 0 1 0 1.06-1.06L8.31 8.75H14ZM6.53 4.53a.75.75 0 0 0-1.06-1.06l-4 4a.75.75 0 0 0 0 1.06l4 4a.75.75 0 0 0 1.06-1.06L3.06 8l3.47-3.47Z"
+            clipRule="evenodd"
+        />
+    </svg>
+);
+export default ArrowChevronLeft;
