7eed551a12a70c3467587e2c8cc0dab8aad8d35c
amje <amje@users.noreply.github.com>
2024-06-04 01:23:33 +0000

    feat: sync icons

--- lib/CircleMinus.tsx
+++ lib/CircleMinus.tsx
@@ -12,7 +12,7 @@ const CircleMinus = (props: SVGProps<SVGSVGElement>) => (
         <path
             fill="currentColor"
             fillRule="evenodd"
-            d="M13.5 8a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0ZM15 8A7 7 0 1 1 1 8a7 7 0 0 1 14 0Zm-9.5-.75a.75.75 0 0 0 0 1.5h5a.75.75 0 0 0 0-1.5h-5Z"
+            d="M13.5 8a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0ZM15 8A7 7 0 1 1 1 8a7 7 0 0 1 14 0Zm-9.5-.75a.75.75 0 1 0 0 1.5h5a.75.75 0 0 0 0-1.5h-5Z"
             clipRule="evenodd"
         />
     </svg>
